9364dd7b939c42121af8c1d5928e6223fcb6b3f5
saibibarbachane <60187657+saibibarbachane@users.noreply.github.com>
2021-10-08 09:15:38 +0200

    Add project files.

---
+++ LMSG3.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LMSG3.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
