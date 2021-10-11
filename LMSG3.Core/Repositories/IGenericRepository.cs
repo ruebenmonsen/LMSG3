86f117b32451dec7c76da953ab84df3c06e108c3
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-10-11 13:50:14 +0200

    Added generic repo interface

---
+++ LMSG3.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSG3.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(int? id);
+        Task<TEntity> FindAsync(int? id);
+        Task<bool> AnyAsync(int id);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        bool Exists(int id);
+    }
+}
