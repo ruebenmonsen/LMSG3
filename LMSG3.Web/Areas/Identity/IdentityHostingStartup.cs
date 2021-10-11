8ff4e7d16a21c994449aaf7ed0de89158523a70f
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-10-11 11:20:46 +0200

    identity scaffolding

---
+++ LMSG3.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LMSG3.Core.Models.Entities;
+using LMSG3.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LMSG3.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LMSG3.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
