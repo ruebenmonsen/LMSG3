8142f1c87d2772d3af061b59239a873e539a7048
Linus Ljung <linus.ljung@protonmail.com>
2021-10-12 09:02:58 +0200

    SeedDate finsihed

--- LMSG3.Data/Migrations/20211011225931_preseed.Designer.cs
+++ LMSG3.Data/Migrations/20211011225931_preseed.Designer.cs
@@ -4,14 +4,16 @@ using LMSG3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LMSG3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211011225931_preseed")]
+    partial class preseed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
