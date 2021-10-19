69e1c9e6cb46daadf0abe0f03377e1e624c61c10
Linus Ljung <linus.ljung@protonmail.com>
2021-10-19 14:51:25 +0200

    Remove literature seed data

--- LMSG3.Data/SeedData.cs
+++ LMSG3.Data/SeedData.cs
@@ -31,15 +31,11 @@ namespace LMSG3.Data
             using (var db = services.GetRequiredService<ApplicationDbContext>())
             {
                 // TODO: Check all entites or just one?
-                if (await db.Literatures.AnyAsync()) return;
+                if (await db.Courses.AnyAsync()) return;
 
                 //Common
                 fake = new Faker("en");
                 ti = new CultureInfo("en-US", false).TextInfo;
-
-                // API
-                var letertures = GetLeterature();
-                await db.AddRangeAsync(letertures);
 
                 // MVC
                 defaultPassword = "Abc123!";
@@ -73,77 +69,16 @@ namespace LMSG3.Data
                 // Too much data will slow the program
                 await AddBogusMvcDataAsync(db);
 
-
                 // Save to DB
                 await db.SaveChangesAsync();
             }
-        }
-
-        private static List<Literature> GetLeterature()
-        {
-
-            var literatures = new List<Literature>();
-            DateTime dt2 = new DateTime(2015, 12, 31);
-            var levels = new List<string> { "Advanced", "Beginner", "Expert", "Intermediate" };
-            var leteraTypes = new List<string> { "Drama", "Fable", "Fiction", "Poetry", "Science", "IT"};
-            var random = new Random();
-            for (int i = 0; i < 200; i++)
-            {
-                int index = random.Next(levels.Count);
-                var title = fake.Lorem.Sentence();
-                var description = fake.Lorem.Paragraph();
-                var releaseDate = fake.Date.Between(dt2, DateTime.Now);
-                var leterature = new Literature
-                {
-                    Title = title,
-                    Description = description,
-                    ReleaseDate = releaseDate.Date,
-                    Authors = new LiteratureAuthor[]
-                    {
-                        new LiteratureAuthor
-                        {
-                            FirstName = fake.Name.FirstName(),
-                            LastName = fake.Name.LastName(),
-                            DateOfBirth = fake.Date.Between(new DateTime(1940, 12, 31), new DateTime(1995, 12, 31)).Date
-                        },
-                        new LiteratureAuthor
-                        {
-                            FirstName = fake.Name.FirstName(),
-                            LastName = fake.Name.LastName(),
-                            DateOfBirth = fake.Date.Between(new DateTime(1940, 12, 31), new DateTime(1995, 12, 31)).Date
-                        },
-                         new LiteratureAuthor
-                        {
-                            FirstName = fake.Name.FirstName(),
-                            LastName = fake.Name.LastName(),
-                            DateOfBirth = fake.Date.Between(new DateTime(1940, 12, 31).Date, new DateTime(1995, 12, 31).Date).Date
-                        }
-
-                    },
-                    Subject = new Subject
-                    {
-                        Name = fake.Lorem.Sentence(),
-                    },
-                    LiteratureLevel = new LiteratureLevel
-                    {
-                        Name = levels[index]
-                    },
-                    LiteratureType = new LiteratureType
-                    {
-                        Name = leteraTypes[index]
-                    }
-                };
-                literatures.Add(leterature);
-            }
-
-            return literatures;
         }
 
         private static async Task AddDefaultMvcDataAsync(ApplicationDbContext db)
         {
             // Keep a day between the modules to reduce logic for activities.
             var currentModuleStart = DateTime.Now.AddDays(-7);
-            var currentModuleEnd = currentModuleStart.AddDays(13);
+            var currentModuleEnd = currentModuleStart.AddDays(21);
             var previousModuleStart = currentModuleStart.AddDays(-7);
             var previousModuleEnd = currentModuleStart.AddDays(-1);
             var nextModuleStart = currentModuleEnd.AddDays(1);
@@ -264,10 +199,10 @@ namespace LMSG3.Data
             }
 
             // Default bogus documents
-            var teacherDocuments = GetDocuments(teachers, teacherRole, documentTypes, new List<Course> { defaultCourse }, modules, activities, 50);
+            var teacherDocuments = GetDocuments(teachers, teacherRole, documentTypes, new List<Course> { defaultCourse }, modules, activities, 100);
             await db.AddRangeAsync(teacherDocuments);
 
-            var studentDocuments = GetDocuments(students, studentRole, documentTypes, new List<Course> { defaultCourse }, modules, activities, 75);
+            var studentDocuments = GetDocuments(students, studentRole, documentTypes, new List<Course> { defaultCourse }, modules, activities, 500);
             await db.AddRangeAsync(studentDocuments);
 
         }
