4280c7262d1dd3d21118a54e4526390e546070c2
rosy871 <yalameju@gmail.com>
2021-10-27 14:59:46 +0200

    migration

--- LMSG3.Web/Program.cs
+++ LMSG3.Web/Program.cs
@@ -18,8 +18,8 @@ namespace LMSG3.Web
             {
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<ApplicationDbContext>();
-                //context.Database.EnsureDeleted();
-                //context.Database.Migrate();
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
                 try
                 {
                     SeedData.InitAsync(services).Wait();
